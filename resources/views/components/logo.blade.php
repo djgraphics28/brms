4887c8a3ccdea39ccd324e70d074e7518d363b89
darwin ibay <darwin.i@shoredigitalinc.com>
2024-08-07 22:35:27 +0800

    updates

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,5 +1,5 @@
 <div class="text-xl flex items-center">
     <img src="{{ asset('images/logo1.png') }}" alt="" width="75px">
     <img src="{{ asset('images/logo2.png') }}" alt="" width="75px">
-    <span class="ml-2">BSRSPM<span class="text-primary-500 font-medium"> Barangay Sto. Rosario Silangan</span></span>
+    <span class="ml-2 text-primary-500 font-medium"> Barangay Sto. Rosario Silangan</span>
 </div>
