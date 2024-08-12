0e88feef5bb798ce93246ea1d9fd9e58c7d20682
djgraphics28 <darwin.ibay30@gmail.com>
2024-08-12 08:22:06 +0800

    updates

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -6,7 +6,7 @@
 
 <div {{ $attributes->merge(['class' => 'py-16 text-white bg-primary-500 mb-8 h-72']) }} style="background-image: url('{{ $backgroundImage }}'); background-size: cover; background-position: center;">
   <x-container>
-    <h1 class="text-4xl">{{ $title ?? $slot }}</h1>
+    {{-- <h1 class="text-4xl">{{ $title ?? $slot }}</h1> --}}
 
     @if ($afterTitle)
       <div class="mt-4">
