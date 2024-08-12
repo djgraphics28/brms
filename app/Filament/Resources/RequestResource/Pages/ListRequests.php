b210ac5a63debfe0f23307bff6e75f2c2cecfdb8
djgraphics28 <darwin.ibay30@gmail.com>
2024-08-12 08:19:45 +0800

    updates

--- app/Filament/Resources/RequestResource/Pages/ListRequests.php
+++ app/Filament/Resources/RequestResource/Pages/ListRequests.php
@@ -14,7 +14,7 @@ class ListRequests extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 
